78f95c6d9703238fca947a535f8bc12ada4f16a1
illler <0250901@gmail.com>
2022-10-03 15:52:36 +0300

    Задание 8 по предмету ССП (сложный вариант)

---
+++ src/Exc/Exc_8.java
@@ -0,0 +1,100 @@
+package Exc;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.Arrays;
+
+abstract class Exc_8 extends Exc_all{
+
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+class Worker extends Exc_8{
+
+    private final Connection con;
+
+    Worker() throws SQLException{
+        DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+        String mysqlUrl1 = "jdbc:mysql://localhost/Exc8";
+        con = DriverManager.getConnection(mysqlUrl1, "root", Config.getPassword());
+        System.out.println("Успешно подключились к бд");
+    }
+
+    @Override
+    public void sql_show(String a) throws SQLException {
+        super.sql_show(a);
+    }
+
+    @Override
+    public void drop(String a) throws SQLException {
+        super.drop(a);
+    }
+
+    @Override
+    public void excel(String mysqlUrl1) throws ClassNotFoundException, SQLException, IOException {
+        super.excel(mysqlUrl1);
+    }
+
+    @Override
+    public void sql_create() throws SQLException {
+        Statement statement1 = con.createStatement();
+        String query = "CREATE TABLE IF NOT EXISTS " + getName_table() + " (Name varchar(255), Age varchar(255), Salary " +
+                "varchar(255))";
+        statement1.executeUpdate(query);
+        ResultSet rs1 = statement1.executeQuery("Show tables");
+        System.out.println("Таблицы из текущей бд");
+        while (rs1.next()) {
+            System.out.print(rs1.getString(1));
+            System.out.println();
+        }
+        System.out.println();
+    }
+    private double salary;
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void section() throws SQLException{
+        System.out.print("Введите имя сотрудника: ");
+        setName(sc.nextLine());
+        System.out.print("Введите возраст сотрудника: ");
+        setAge(sc.nextInt());
+        System.out.print("Введите зарплату сотрудника: ");
+        setSalary(sc.nextDouble());
+        String query = "insert into " + getName_table() + " values (?, ?, ?)";
+        PreparedStatement statement = con.prepareStatement(query);
+        statement.setString(1, getName());
+        statement.setInt(2, getAge());
+        statement.setDouble(3, getSalary());
+        statement.executeUpdate();
+        ResultSet rs = statement.executeQuery("select * from " + getName_table());
+        while (rs.next()){
+            System.out.print(Arrays.toString(rs.getString(1).split("  ")));
+            System.out.print(Arrays.toString(rs.getString(2).split("  ")));
+            System.out.print(Arrays.toString(rs.getString(3).split("  ")));
+            System.out.println();
+        }
+    }
+}
